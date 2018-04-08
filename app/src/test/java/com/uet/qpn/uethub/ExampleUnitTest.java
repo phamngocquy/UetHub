4eca0d3f3ab1e66c7479c9b6b7c1e40c0b8b6c22
phamngocquy <phamngocquy97@gmail.com>
2018-04-08 15:53:04 +0700

    update

--- app/src/test/java/com/uet/qpn/uethub/ExampleUnitTest.java
+++ app/src/test/java/com/uet/qpn/uethub/ExampleUnitTest.java
@@ -1,5 +1,15 @@
 package com.uet.qpn.uethub;
 
+import android.content.Context;
+import android.net.Uri;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -13,5 +23,36 @@ public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() throws Exception {
         assertEquals(4, 2 + 2);
+    }
+
+    @Test
+    public void getNewsData() {
+       /* Context context = new MainActivity();
+        RequestQueue requestQueue = Volley.newRequestQueue(context);*/
+        Uri uri = new Uri.Builder()
+                .scheme("http")
+                .authority("localhost")
+                .appendPath("api")
+                .appendPath("v1")
+                .appendPath("news")
+                .appendPath("getEntitiesByNews")
+                .appendQueryParameter("news","FIT")
+                .appendQueryParameter("page","0").build();
+        System.out.println(uri.toString());
+
+
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, "http://localhost:8080/api/v1/news/getEntitiesByNews?news=fit&page=0", new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
+
+       // requestQueue.add(stringRequest);
     }
 }
