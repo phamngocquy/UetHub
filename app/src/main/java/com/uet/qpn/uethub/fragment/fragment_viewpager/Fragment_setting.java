76cda1411afb4e30807531ee920289a48fbd4195
phamngocquy <phamngocquy97@gmail.com>
2018-05-07 22:54:26 +0700

    add facebook SDK

--- app/src/main/java/com/uet/qpn/uethub/fragment/fragment_viewpager/Fragment_setting.java
+++ app/src/main/java/com/uet/qpn/uethub/fragment/fragment_viewpager/Fragment_setting.java
@@ -1,24 +1,123 @@
 package com.uet.qpn.uethub.fragment.fragment_viewpager;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.Profile;
+import com.facebook.appevents.AppEventsLogger;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 import com.uet.qpn.uethub.R;
 
+import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Currency;
+
 public class Fragment_setting extends Fragment {
+    private CallbackManager callbackManager;
 
     public Fragment_setting() {
-
     }
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_setting, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        FacebookSdk.sdkInitialize(getContext().getApplicationContext());
+        AppEventsLogger.activateApp(getContext());
+
+        callbackManager = CallbackManager.Factory.create();
+
+        ImageView imgAvt = view.findViewById(R.id.imgAvt);
+
+        LoginButton loginButton = view.findViewById(R.id.login_button);
+        loginButton.setReadPermissions("email");
+        // If using in a fragment
+        loginButton.setFragment(this);
+
+        // Callback registration
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                // App code
+            }
+
+            @Override
+            public void onCancel() {
+                // App code
+            }
+
+            @Override
+            public void onError(FacebookException exception) {
+                // App code
+            }
+        });
+
+
+        callbackManager = CallbackManager.Factory.create();
+
+        LoginManager.getInstance().registerCallback(callbackManager,
+                new FacebookCallback<LoginResult>() {
+                    @Override
+                    public void onSuccess(LoginResult loginResult) {
+                        // App code
+                    }
+
+                    @Override
+                    public void onCancel() {
+                        // App code
+                    }
+
+                    @Override
+                    public void onError(FacebookException exception) {
+                        // App code
+                    }
+                });
+
+
+        AccessToken accessToken = AccessToken.getCurrentAccessToken();
+        boolean isLoggedIn = accessToken == null;
+        boolean isExpired = accessToken.isExpired();
+        LoginManager.getInstance().logInWithReadPermissions(this, Collections.singletonList("public_profile"));
+
+        if (!isExpired) {
+            Profile profile = Profile.getCurrentProfile();
+            if (profile != null) {
+                Log.d("name", profile.getFirstName());
+                Uri uri = profile.getProfilePictureUri(100, 100);
+                Log.d("avatar",uri.toString());
+                imgAvt.setImageURI(uri);
+
+            }
+        }
+        // Log.d("name", Profile.getCurrentProfile().getFirstName());
+
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }
