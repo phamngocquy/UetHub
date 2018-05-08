e04722aafb746f1475e37b2f3edbbf0f6235fabb
phamngocquy <phamngocquy97@gmail.com>
2018-05-08 17:29:17 +0700

    update facebook

--- app/src/main/java/com/uet/qpn/uethub/fragment/fragment_viewpager/Fragment_setting.java
+++ app/src/main/java/com/uet/qpn/uethub/fragment/fragment_viewpager/Fragment_setting.java
@@ -1,8 +1,10 @@
 package com.uet.qpn.uethub.fragment.fragment_viewpager;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -10,27 +12,41 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebChromeClient;
+import android.webkit.WebViewClient;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.facebook.AccessToken;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
 import com.facebook.Profile;
 import com.facebook.appevents.AppEventsLogger;
 import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
+import com.squareup.picasso.Picasso;
 import com.uet.qpn.uethub.R;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
 import java.math.BigDecimal;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.Currency;
 
+import de.hdodenhof.circleimageview.CircleImageView;
+
 public class Fragment_setting extends Fragment {
     private CallbackManager callbackManager;
+    private CircleImageView imgAvt;
+    private TextView txtUserName;
 
     public Fragment_setting() {
     }
@@ -49,7 +65,8 @@ public class Fragment_setting extends Fragment {
 
         callbackManager = CallbackManager.Factory.create();
 
-        ImageView imgAvt = view.findViewById(R.id.imgAvt);
+        imgAvt = view.findViewById(R.id.imgAvt);
+        txtUserName = view.findViewById(R.id.txtUserName);
 
         LoginButton loginButton = view.findViewById(R.id.login_button);
         loginButton.setReadPermissions("email");
@@ -61,6 +78,7 @@ public class Fragment_setting extends Fragment {
             @Override
             public void onSuccess(LoginResult loginResult) {
                 // App code
+
             }
 
             @Override
@@ -81,7 +99,30 @@ public class Fragment_setting extends Fragment {
                 new FacebookCallback<LoginResult>() {
                     @Override
                     public void onSuccess(LoginResult loginResult) {
-                        // App code
+
+                        Profile profile = Profile.getCurrentProfile();
+
+                        if (profile != null) {
+                            final Uri uri = profile.getProfilePictureUri(150, 150);
+                            Picasso.get().load(uri).into(imgAvt);
+                            GraphRequest request = GraphRequest.newMeRequest(loginResult.getAccessToken(), new GraphRequest.GraphJSONObjectCallback() {
+                                @Override
+                                public void onCompleted(JSONObject object, GraphResponse response) {
+
+                                    try {
+                                        txtUserName.setText(object.getString("name"));
+                                    } catch (JSONException e) {
+                                        e.printStackTrace();
+                                    }
+                                }
+                            });
+
+                            Bundle parameters = new Bundle();
+                            parameters.putString("fields", "id,name,email,picture");
+                            request.setParameters(parameters);
+                            request.executeAsync();
+
+                        }
                     }
 
                     @Override
@@ -95,23 +136,11 @@ public class Fragment_setting extends Fragment {
                     }
                 });
 
-
         AccessToken accessToken = AccessToken.getCurrentAccessToken();
         boolean isLoggedIn = accessToken == null;
         boolean isExpired = accessToken.isExpired();
         LoginManager.getInstance().logInWithReadPermissions(this, Collections.singletonList("public_profile"));
 
-        if (!isExpired) {
-            Profile profile = Profile.getCurrentProfile();
-            if (profile != null) {
-                Log.d("name", profile.getFirstName());
-                Uri uri = profile.getProfilePictureUri(100, 100);
-                Log.d("avatar",uri.toString());
-                imgAvt.setImageURI(uri);
-
-            }
-        }
-        // Log.d("name", Profile.getCurrentProfile().getFirstName());
 
     }
 
