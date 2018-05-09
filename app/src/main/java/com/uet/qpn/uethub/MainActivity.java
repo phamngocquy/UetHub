ec422e86de13906a7251aefe0346b3f5314140d7
phamngocquy <phamngocquy97@gmail.com>
2018-05-09 22:23:43 +0700

    edit view setting

--- app/src/main/java/com/uet/qpn/uethub/MainActivity.java
+++ app/src/main/java/com/uet/qpn/uethub/MainActivity.java
@@ -34,7 +34,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        setTheme(R.style.Theme_Design_Light_NoActionBar);
+        setTheme(R.style.CustomTheme);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         checkPermission();
